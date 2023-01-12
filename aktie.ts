860e1acbf0d9026213a7db61bf5f8089d3f6c491
InsomniaSoul <122524898+InsomniaSoul@users.noreply.github.com>
2023-01-12 11:16:05 +0100

    1 file modified, 1 added

---
+++ aktie.ts
@@ -0,0 +1,38 @@
+import { Gegenstand } from "./gegenstand";
+
+export class aktie extends Gegenstand {
+
+  constructor(private unternehmen: string, private nennwert: number,) {
+
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
--- schmuck.ts
+++ schmuck.ts
@@ -2,10 +2,26 @@ import { Gegenstand } from "./gegenstand"; //von Stackblitz automat. generiert
 
 export class Schmuck extends Gegenstand {
 
-  constructor(id: number, wert: number, //todo) {
-	//todo
+  constructor(private bezeichnung: string, public id: number, public wert: number,) {
+
+  }
+
+  get bezeichnung() {
+    return this.bezeichnung
+  }
+  set bezeichnung(bezeichnung:void) {
+    this.bezeichnung = this.bezeichnung
+  }
+
+  toString(): string {
+    /**
+     * Datentyp der Variablen 'text' muss nicht zwangsläufig festgelegt werden.
+     * Er ergibt sich andernfalls auch durch die Zuweisung.
+     */
+    let text: string = "\nID: " + this.id;
+    text += "\nVersicherungswert: " + this.wert;
+    return text;
   }
   
-  //todo
 
 }
